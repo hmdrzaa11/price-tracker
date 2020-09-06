bcfd819ab6f03501f03801f7780530856191034e
Hamid Reza <hmdrzaa11@gmail.com>
2020-09-07 00:53:05 +0430

    Removed Some Comment

--- parser.js
+++ parser.js
@@ -5,13 +5,13 @@ let [_, __, url, minPrice] = process.argv;
 async function checkPrice() {
   let priceString = await nightmare
     .goto(url)
-    .wait("#priceblock_ourprice") //We await till this element show up into the DOM
+    .wait("#priceblock_ourprice") //await till this element show up into the DOM
     .evaluate(() => {
       let price = document.getElementById("priceblock_ourprice").innerText;
-      //do not put any console..log here because this code is running some where else just return the result
+
       return price;
     })
-    .end(); //we have to end it
+    .end();
   let price = parseFloat(priceString.replace(/\$|,/g, ""));
   if (price < parseFloat(minPrice)) {
     console.log("its cheap ");
